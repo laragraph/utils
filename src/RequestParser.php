ce583344319dd08de67d53e418fbe4754c2ee804
spawnia <spawnia@users.noreply.github.com>
2024-02-23 11:00:09 +0000

    Apply php-cs-fixer changes

--- src/RequestParser.php
+++ src/RequestParser.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Safe\Exceptions\JsonException;
-
 use function Safe\json_decode;
 
 /** Follows https://github.com/graphql/graphql-over-http/blob/main/spec/GraphQLOverHTTP.md. */
